5ab26e43a924fea5b15e70c4738f0996317cebf4
Трубкин Сергей <trubkinsa@gmail.com>
2020-07-07 20:38:08 +0300

    7.3 Перемещает функцию отрисовки меток в модуль pin.js

--- js/map.js
+++ js/map.js
@@ -1,52 +1,8 @@
 'use strict';
 
 (function () {
-  var FILTER_ANY = 'any';
   var Nodes = window.const.Nodes;
   var KeyCode = window.const.KeyCode;
-  var usersData = [];
-
-  var renderPins = function (users) {
-    var activeUsers = users.slice(0, Math.min(users.length, window.const.USER_COUNT));
-    var pins = window.utils.createElements(activeUsers, window.pin.createTemplate);
-    var cards = window.utils.createElements(activeUsers, window.card.createTemplate);
-
-    for (var i = 0; i < activeUsers.length; i++) {
-      window.card.openPopup(pins.children[i], cards.children[i], i);
-    }
-
-    Nodes.MAP_PINS_BLOCK.appendChild(pins);
-    window.form.activationForm(Nodes.MAP_FILTERS, true);
-  };
-
-  // отрисовка меток на карте
-  var updatePins = function (filter) {
-
-    if (!usersData.length && !filter) {
-
-      window.backend.load(function (users) {
-        usersData = users;
-        renderPins(usersData);
-      }, function () {});
-
-    } else if (usersData.length && filter) {
-      window.pin.remove();
-      var filteredUsers = [];
-
-      if (filter === FILTER_ANY) {
-        filteredUsers = usersData;
-      } else {
-        filteredUsers = usersData.filter(function (it) {
-          return it.offer.type === filter;
-        });
-
-      }
-      renderPins(filteredUsers);
-    } else {
-      return;
-    }
-
-  };
 
   // функция активация/деактивация карты. *true/false*
   var activationMap = function (stat) {
@@ -65,17 +21,17 @@
     Nodes.MAP_PIN_MAIN.removeEventListener('keydown', window.map.clickMainPinHandler);
   };
 
+  // обработчик клика по главной метке
   var clickMainPinHandler = function (evt) {
-    if (evt.button === KeyCode.MOUSE_BUTTON_LEFT || evt.keyCode === KeyCode.ENTER) {
+    if (evt.button === KeyCode.MOUSE_BUTTON_LEFT || evt.keyCode === KeyCode.ENTER && !window.usersData.length) {
       window.form.setAddressPin('move');
-      window.map.updatePins();
+      window.pin.addOnMap();
       window.map.activationMap(true);
-      window.form.activationForm(Nodes.const.FORM, true);
+      window.form.activationForm(Nodes.FORM, true);
     }
   };
 
   window.map = {
-    updatePins: updatePins,
     activationMap: activationMap,
     clickMainPinHandler: clickMainPinHandler,
   };
--- js/pin.js
+++ js/pin.js
@@ -24,6 +24,20 @@
     Nodes.MAP_PIN_MAIN.style.top = window.const.PinLocation.MAIN_TOP + 'px';
   };
 
+  // отрисовка меток на карте
+  var render = function (users) {
+    var activeUsers = users.slice(0, Math.min(users.length, window.const.USER_COUNT));
+    var pins = window.utils.createElements(activeUsers, window.pin.createTemplate);
+    var cards = window.utils.createElements(activeUsers, window.card.createTemplate);
+
+    for (var i = 0; i < activeUsers.length; i++) {
+      window.card.openPopup(pins.children[i], cards.children[i], i);
+    }
+
+    Nodes.MAP_PINS_BLOCK.appendChild(pins);
+    window.form.activationForm(Nodes.MAP_FILTERS, true);
+  };
+
   var remove = function () {
     var pins = document.querySelectorAll('.map__pin');
     var card = document.querySelector('.map__card');
@@ -39,9 +53,27 @@
     }
   };
 
+  // var onError = function (message) {
+  //   console.log(message);
+  // };
+
+  var onSuccess = function (users) {
+    window.usersData = users;
+    render(window.usersData);
+  };
+
+  var addOnMap = function () {
+
+    if (!window.usersData.length) {
+      window.backend.load(onSuccess, function () {});
+    }
+  };
+
   window.pin = {
     createTemplate: createTemplate,
     resetPosition: resetPosition,
+    render: render,
     remove: remove,
+    addOnMap: addOnMap,
   };
 })();
